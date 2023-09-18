1a2e2988a5634e0982d60a1b233716a4e579bb8b
ankitraju123 <ankitrchauhan1997@gmail.com>
2023-09-18 16:43:33 +0530

    add

--- pages/latex.js
+++ pages/latex.js
@@ -104,7 +104,7 @@ const LatexPage = () => {
       <br />
       <br />
       <br />
-      <div className='latex'>
+      <div className='latex p-4'>
       {htmlContent && (
         <MathJaxContext>
           <MathJax key={mathJaxKey}>
